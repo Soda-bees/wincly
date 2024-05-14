58cd96923dfdfd9f3b2d1d765720e81d55ac89f4
SimationStudios <brian@simationstudios.com>
2024-05-14 05:03:57 -0700

    facebook authentication integration

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Image,
   Platform,
@@ -6,30 +6,36 @@ import {
   Text,
   TouchableOpacity,
   View,
-  PermissionsAndroid
+  PermissionsAndroid,
 } from 'react-native';
 import images from '../../services/utilities/images';
-import { styles } from './style';
+import {styles} from './style';
 import Button from '../../components/Button';
 import auth from '@react-native-firebase/auth';
-import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { sha256 } from 'react-native-sha256';
-import { getFcmToken } from '../../services/config/notificationServices';
-import { useDispatch } from 'react-redux';
-import { ActivityIndicator } from 'react-native-paper';
-import { sizes } from '../../services';
-import { setShowTutorialFalse } from '../../store/showTutorial';
-import { handleTrue } from '../../store/isSignedInSlice';
-import { handleAddUserDetails } from '../../store/userDetailsSlice';
-import socket from "../../services/config/io"
+import {
+  LoginManager,
+  AccessToken,
+  GraphRequest,
+  GraphRequestManager,
+  LoginButton,
+} from 'react-native-fbsdk-next';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {sha256} from 'react-native-sha256';
+import {getFcmToken} from '../../services/config/notificationServices';
+import {useDispatch} from 'react-redux';
+import {ActivityIndicator} from 'react-native-paper';
+import {sizes} from '../../services';
+import {setShowTutorialFalse} from '../../store/showTutorial';
+import {handleTrue} from '../../store/isSignedInSlice';
+import {handleAddUserDetails} from '../../store/userDetailsSlice';
+import socket from '../../services/config/io';
 import axios from 'axios';
 import backendURL from '../../services/config/backendURL';
 
-export default function LandingPage({ navigation }) {
-  const dispatch = useDispatch()
-  const [deviceToken, setDeviceToken] = useState()
-  const [googleLoader, setGoogleLoader] = useState(false)
+export default function LandingPage({navigation}) {
+  const dispatch = useDispatch();
+  const [deviceToken, setDeviceToken] = useState();
+  const [googleLoader, setGoogleLoader] = useState(false);
 
   useEffect(() => {
     GoogleSignin.configure({
@@ -40,17 +46,15 @@ export default function LandingPage({ navigation }) {
     });
   }, []);
 
-
-
   const getTokanForDB = async () => {
-    const token = await getFcmToken()
+    const token = await getFcmToken();
     // console.log('token=--==->', token);
-    setDeviceToken(token)
-  }
+    setDeviceToken(token);
+  };
 
   useEffect(() => {
-    getTokanForDB()
-  }, [])
+    getTokanForDB();
+  }, []);
 
   const handleSignUp = () => {
     navigation.navigate('Signup', deviceToken);
@@ -59,70 +63,118 @@ export default function LandingPage({ navigation }) {
     navigation.navigate('Signin', deviceToken);
   };
 
-  const handleFacebook = async () => {
-    if (Platform.OS == 'android') {
-      const result = await LoginManager.logInWithPermissions([
-        'public_profile',
-        'email',
-      ]);
+  // const handleFacebook = async () => {
+  //   if (Platform.OS == 'android') {
+  //     const result = await LoginManager.logInWithPermissions([
+  //       'public_profile',
+  //       'email',
+  //     ]);
 
-      if (result.isCancelled) {
-        throw 'User cancelled the login process';
-      }
+  //     if (result.isCancelled) {
+  //       throw 'User cancelled the login process';
+  //     }
 
-      // Once signed in, get the users AccessToken
-      const data = await AccessToken.getCurrentAccessToken();
+  //     // Once signed in, get the users AccessToken
+  //     const data = await AccessToken.getCurrentAccessToken();
 
-      if (!data) {
-        throw 'Something went wrong obtaining access token';
-      }
+  //     if (!data) {
+  //       throw 'Something went wrong obtaining access token';
+  //     }
 
-      // Create a Firebase credential with the AccessToken
-      const facebookCredential = auth.FacebookAuthProvider.credential(
-        data.accessToken,
-      );
+  //     // Create a Firebase credential with the AccessToken
+  //     const facebookCredential = auth.FacebookAuthProvider.credential(
+  //       data.accessToken,
+  //     );
 
-      // Sign-in the user with the credential
-      let user = auth().currentUser;
-      console.log(user, '----->>');
-      return auth().signInWithCredential(facebookCredential);
-    } else {
-      const nonce = '123456';
-      const nonceSha256 = await sha256(nonce);
+  //     // Sign-in the user with the credential
+  //     let user = auth().currentUser;
+  //     console.log(user, '----->>');
+  //     return auth().signInWithCredential(facebookCredential);
+  //   } else {
+  //     const nonce = '123456';
+  //     const nonceSha256 = await sha256(nonce);
 
-      console.log(nonceSha256, '---->>>>s');
-      // Attempt login with permissions and limited login
-      const result = await LoginManager.logInWithPermissions(
-        ['public_profile', 'email'],
-        'limited',
-        nonceSha256,
-      );
+  //     console.log(nonceSha256, '---->>>>s');
+  //     // Attempt login with permissions and limited login
+  //     const result = await LoginManager.logInWithPermissions(
+  //       ['public_profile', 'email'],
+  //       'limited',
+  //       nonceSha256,
+  //     );
 
-      if (result.isCancelled) {
-        throw 'User cancelled the login process';
-      }
+  //     if (result.isCancelled) {
+  //       throw 'User cancelled the login process';
+  //     }
 
-      // Once signed in, get the users AuthenticationToken
-      const data = await AuthenticationToken.getAuthenticationTokenIOS();
+  //     // Once signed in, get the users AuthenticationToken
+  //     const data = await AuthenticationToken.getAuthenticationTokenIOS();
 
-      if (!data) {
-        throw 'Something went wrong obtaining authentication token';
-      }
+  //     if (!data) {
+  //       throw 'Something went wrong obtaining authentication token';
+  //     }
 
-      // Create a Firebase credential with the AuthenticationToken
-      // and the nonce (Firebase will validates the hash against the nonce)
-      const facebookCredential = auth().FacebookAuthProvider.credential(
-        data.authenticationToken,
-        nonce,
-      );
+  //     // Create a Firebase credential with the AuthenticationToken
+  //     // and the nonce (Firebase will validates the hash against the nonce)
+  //     const facebookCredential = auth().FacebookAuthProvider.credential(
+  //       data.authenticationToken,
+  //       nonce,
+  //     );
 
-      // Sign-in the user with the credential
-      return auth().signInWithCredential(facebookCredential).then(() => {
-        let user = auth().currentUser;
-        console.log(user.displayName, '----->>');
-      })
-    }
+  //     // Sign-in the user with the credential
+  //     return auth().signInWithCredential(facebookCredential).then(() => {
+  //       let user = auth().currentUser;
+  //       console.log(user.displayName, '----->>');
+  //     })
+  //   }
+  // };
+
+  const handleFacebook = () => {
+    LoginManager.logInWithPermissions(['public_profile']).then(
+      function (result) {
+        if (result.isCancelled) {
+          alert('Login was cancelled');
+        } else {
+          AccessToken.getCurrentAccessToken().then(data => {
+            const accessToken = data.accessToken.toString();
+
+            const responseInfoCallback = (error, result) => {
+              if (error) {
+                console.log(error);
+                alert('Error fetching data: ' + error.toString());
+              } else {
+                console.log('hellllllllllo',result);
+                alert('Welcome: ' + result.name);
+              }
+            };
+
+            const infoRequest = new GraphRequest(
+              '/me',
+              {
+                accessToken: accessToken,
+                parameters: {
+                  fields: {
+                    string: 'email,name,first_name,middle_name,last_name',
+                  },
+                },
+              },
+              responseInfoCallback,
+            );
+
+            // Start the graph request.
+            new GraphRequestManager().addRequest(infoRequest).start();
+          });
+        }
+      },
+      function (error) {
+        console.log('Login fail with error: ' + error);
+      },
+    );
   };
+
+  // const handleLogout = () => {
+  //   LoginManager.logOut();
+  //   alert('Logged out successfully');
+  // };
 
   const handleGoogle = async () => {
     try {
@@ -130,13 +182,13 @@ export default function LandingPage({ navigation }) {
         await GoogleSignin.hasPlayServices({
           showPlayServicesUpdateDialog: true,
         });
-        const { idToken } = await GoogleSignin.signIn();
+        const {idToken} = await GoogleSignin.signIn();
         const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
         await auth().signInWithCredential(googleCredential);
       } else {
         // IOS
-        const { idToken } = await GoogleSignin.signIn();
+        const {idToken} = await GoogleSignin.signIn();
         const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
         await auth().signInWithCredential(googleCredential);
@@ -145,26 +197,26 @@ export default function LandingPage({ navigation }) {
       let user = auth().currentUser;
       // console.log(user.displayName, '----->>');
       console.log('User Detail', user);
-      handleGoogleSignup(user)
+      handleGoogleSignup(user);
       // alert(`Welcome ${user.displayName}`);
     } catch (error) {
       console.error('Error signing in with Google:', error);
     }
   };
 
-  const handleGoogleSignup = async (user) => {
+  const handleGoogleSignup = async user => {
     try {
-      setGoogleLoader(true)
-      const { data } = await axios.post(
+      setGoogleLoader(true);
+      const {data} = await axios.post(
         backendURL + 'api/wincly/checkEmailGoogle',
         {
           deviceToken,
           email: user?.email,
         },
-      )
+      );
       if (data?.status == 200) {
-        await revokeGoogleAccess()
-        setGoogleLoader(false)
+        await revokeGoogleAccess();
+        setGoogleLoader(false);
         navigation.navigate('PhoneVerification', {
           userData: {
             username: user?.displayName,
@@ -174,26 +226,29 @@ export default function LandingPage({ navigation }) {
             userStatus: 'Online',
             deviceToken,
             loginWith: 'google',
-            profile: user?.photoURL
+            profile: user?.photoURL,
           },
         });
       } else {
-        setGoogleLoader(false)
-        dispatch(setShowTutorialFalse())
-        console.log("already use need to move home");
+        setGoogleLoader(false);
+        dispatch(setShowTutorialFalse());
+        console.log('already use need to move home');
         dispatch(handleTrue());
         dispatch(handleAddUserDetails(data?.existingEmail));
         handleSendDataForServer(data?.existingEmail);
       }
-
     } catch (error) {
-      setGoogleLoader(false)
+      setGoogleLoader(false);
       console.log(error);
     }
-  }
+  };
 
   const handleSendDataForServer = data => {
-    const userData = { username: data?.username, _id: data?._id, userStatus: "Online" };
+    const userData = {
+      username: data?.username,
+      _id: data?._id,
+      userStatus: 'Online',
+    };
     socket.emit('set user', userData);
     socket.connect();
   };
@@ -228,25 +283,61 @@ export default function LandingPage({ navigation }) {
                 Platform.OS == 'ios' ? styles.btnTopIOS : styles.btnTop,
                 styles.row,
               ]}>
-              <TouchableOpacity
-              // onPress={handleFacebook}
-              >
+              <TouchableOpacity onPress={handleFacebook}>
                 <View style={[styles.darkBtn, styles.row2]}>
                   <Image source={images.fb} style={styles.fb} />
                   <Text style={styles.darkBtnText}>Facebook</Text>
                 </View>
               </TouchableOpacity>
-              <TouchableOpacity
-                onPress={handleGoogle}
-              >
-                <View style={[styles.greenBtn, styles.row2]}>
-                  {
-                    googleLoader ?
-                      <ActivityIndicator size={21} color='black'
-                      />
-                      :
-                      <Image source={images.google} style={styles.google} />
+              {/* <LoginButton
+                onLoginFinished={(error, result) => {
+                  if (error) {
+                    alert('login has error: ' + result.error);
+                  } else if (result.isCancelled) {
+                    alert('login is cancelled.');
+                  } else {
+                    AccessToken.getCurrentAccessToken().then(data => {
+                      let accessToken = data.accessToken;
+                      alert(accessToken.toString());
+
+                      const responseInfoCallback = (error, result) => {
+                        if (error) {
+                          console.log(error);
+                          alert('Error fetching data: ' + error.toString());
+                        } else {
+                          console.log(result);
+                          alert('Success fetching data: ' + result.toString());
+                        }
+                      };
+
+                      const infoRequest = new GraphRequest(
+                        '/me',
+                        {
+                          accessToken: accessToken,
+                          parameters: {
+                            fields: {
+                              string:
+                                'email,name,first_name,middle_name,last_name',
+                            },
+                          },
+                        },
+                        responseInfoCallback,
+                      );
+
+                      // Start the graph request.
+                      new GraphRequestManager().addRequest(infoRequest).start();
+                    });
                   }
+                }}
+                onLogoutFinished={() => alert('logout.')}
+              /> */}
+              <TouchableOpacity onPress={handleGoogle}>
+                <View style={[styles.greenBtn, styles.row2]}>
+                  {googleLoader ? (
+                    <ActivityIndicator size={21} color="black" />
+                  ) : (
+                    <Image source={images.google} style={styles.google} />
+                  )}
                   <Text style={styles.greenBtnText}>Google</Text>
                 </View>
               </TouchableOpacity>
